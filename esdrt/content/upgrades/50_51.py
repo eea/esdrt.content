9c47ccb53f57e60d2f28cd21b99f3df18abad00b
David Batranu <david.batranu@eaudeweb.ro>
2017-04-27 21:39:59 +0300

    [refs #84445] - Faster upgrade step.
    Target only specific Questions.

--- esdrt/content/upgrades/50_51.py
+++ esdrt/content/upgrades/50_51.py
@@ -5,10 +5,25 @@ import plone.api as api
 PROFILE_ID = 'profile-esdrt.content:default'
 
 
+STATES = (
+    'phase1-counterpart-comments',
+    'phase1-draft',
+    'phase1-drafted',
+    'phase1-recalled-lr',
+    'phase2-closed',
+    'phase2-draft',
+    'phase2-drafted',
+    'phase2-recalled-lr',
+)
+
+
+PERMISSION = 'Access contents information'
+
+
 def upgrade(context, logger=None):
     if logger is None:
         from logging import getLogger
-        logger = getLogger('esdrt.content.upgrades.38_39')
+        logger = getLogger('esdrt.content.upgrades.50_51')
     install_workflow(context, logger)
     logger.info('Upgrade steps executed')
 
@@ -16,6 +31,7 @@ def upgrade(context, logger=None):
 def install_workflow(context, logger):
     setup = getToolByName(context, 'portal_setup')
     wtool = getToolByName(context, 'portal_workflow')
+    catalog = getToolByName(context, 'portal_catalog')
 
     wtool.manage_delObjects([
         'esd-question-review-workflow',
@@ -24,5 +40,17 @@ def install_workflow(context, logger):
     setup.runImportStepFromProfile(PROFILE_ID, 'workflow')
     logger.info('Reinstalled Workflows.')
 
-    wtool.updateRoleMappings()
-    logger.info('Security settings updated')
+    brains = catalog(portal_type='Question', review_state=STATES)
+    brains_len = len(brains)
+
+    for idx, brain in enumerate(brains, start=1):
+        content = brain.getObject()
+        current_roles = [
+            r['name'] for r in
+            content.rolesOfPermission(PERMISSION)
+            if r['selected']
+        ]
+        new_roles = list(set(current_roles + ['MSAuthority']))
+        content.manage_permission(PERMISSION, roles=new_roles, acquire=0)
+        content.reindexObjectSecurity()
+        logger.info('Updated %s %s/%s.', brain.getURL(), idx, brains_len)
