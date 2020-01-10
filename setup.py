4348e954d54ee7c716e7f360e905699cba6c8cca
David Batranu <david.batranu@eaudeweb.ro>
2020-01-10 18:22:44 +0200

    Formatting with black + isort

--- setup.py
+++ setup.py
@@ -1,52 +1,56 @@
-from setuptools import setup, find_packages
 import os
 
-version = '1.71.7'
+from setuptools import find_packages
+from setuptools import setup
 
-setup(name='esdrt.content',
-      version=version,
-      description="Content-types for ESD Review Tool",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+version = "1.71.7"
+
+setup(
+    name="esdrt.content",
+    version=version,
+    description="Content-types for ESD Review Tool",
+    long_description=open("README.txt").read()
+    + "\n"
+    + open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='Mikel Larreategi',
-      author_email='mlarreategi@codesyntax.com',
-      url='https://github.com/eea/esdrt.content/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['esdrt'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'five.grok',
-          'plone.app.dexterity [grok, relations]',
-          'plone.namedfile [blobs]',
-          'collective.z3cform.datagridfield',
-          'plone.api',
-          'Products.ATVocabularyManager',
-          'plone.app.versioningbehavior',
-          'plone.app.workflowmanager',
-          'cs.htmlmailer',
-          'collective.deletepermission',
-          'tablib',
-          'python-docx == 0.8.5',
-          'zc.dict',
-          'collective.monkeypatcher',
-          'python-ldap',
-          'Products.LDAPUserFolder',
-          'openpyxl',
-      ],
-      entry_points="""
+    ],
+    keywords="",
+    author="Mikel Larreategi",
+    author_email="mlarreategi@codesyntax.com",
+    url="https://github.com/eea/esdrt.content/",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["esdrt"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "five.grok",
+        "plone.app.dexterity [grok, relations]",
+        "plone.namedfile [blobs]",
+        "collective.z3cform.datagridfield",
+        "plone.api",
+        "Products.ATVocabularyManager",
+        "plone.app.versioningbehavior",
+        "plone.app.workflowmanager",
+        "cs.htmlmailer",
+        "collective.deletepermission",
+        "tablib",
+        "python-docx == 0.8.5",
+        "zc.dict",
+        "collective.monkeypatcher",
+        "python-ldap",
+        "Products.LDAPUserFolder",
+        "openpyxl",
+    ],
+    entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      )
+)
