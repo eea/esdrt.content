64f96f182ee67cdbe23bf1648ee4f2e4904edc7b
David Batranu <david.batranu@eaudeweb.ro>
2020-01-29 19:16:11 +0200

    Black reformatting.

--- esdrt/content/roles/localrolesubscriber.py
+++ esdrt/content/roles/localrolesubscriber.py
@@ -1,7 +1,9 @@
+from plone import api
+
 from Acquisition import aq_inner
 from Acquisition import aq_parent
-from plone import api
 from esdrt.content.reviewfolder import IReviewFolder
+
 
 def grant_local_roles(context):
     """ add local roles to the groups when adding an observation
@@ -12,32 +14,33 @@ def grant_local_roles(context):
     parent = aq_parent(aq_inner(context))
     if IReviewFolder.providedBy(parent):
         applyes_to.append(parent)
-    
+
     context.__ac_local_roles_block__ = True
-            
+
     for obj in applyes_to:
         api.group.grant_roles(
-            groupname='extranet-esd-ghginv-sr-%s-%s' % (sector, country),
-            roles=['ReviewerPhase1'],
+            groupname="extranet-esd-ghginv-sr-%s-%s" % (sector, country),
+            roles=["ReviewerPhase1"],
             obj=obj,
         )
         api.group.grant_roles(
-            groupname='extranet-esd-ghginv-qualityexpert-%s' % sector,
-            roles=['QualityExpert'],
+            groupname="extranet-esd-ghginv-qualityexpert-%s" % sector,
+            roles=["QualityExpert"],
             obj=obj,
         )
         api.group.grant_roles(
-            groupname='extranet-esd-esdreview-reviewexp-%s-%s' % (sector, country),
-            roles=['ReviewerPhase2'],
+            groupname="extranet-esd-esdreview-reviewexp-%s-%s"
+            % (sector, country),
+            roles=["ReviewerPhase2"],
             obj=obj,
         )
         api.group.grant_roles(
-            groupname='extranet-esd-esdreview-leadreview-%s' % country,
-            roles=['LeadReviewer'],
+            groupname="extranet-esd-esdreview-leadreview-%s" % country,
+            roles=["LeadReviewer"],
             obj=obj,
         )
         api.group.grant_roles(
-            groupname='extranet-esd-countries-msa-%s' % country,
-            roles=['MSAuthority'],
+            groupname="extranet-esd-countries-msa-%s" % country,
+            roles=["MSAuthority"],
             obj=obj,
         )
